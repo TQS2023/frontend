72626a5f9dd5d2d063965a10adcc189083c3e9dc
LuisCarlos104171 <luiscarlos.c.afonso@ua.pt>
2023-06-22 14:36:03 +0100

    Connected with the backend

---
+++ backend.ts
@@ -0,0 +1,4 @@
+const BASE_URL = 'http://localhost:8080/api';
+
+export const ALL_PRODUCTS_API = BASE_URL + '/product/all';
+export const PRODUCT_API = BASE_URL + '/product/';
--- types.ts
+++ types.ts
@@ -2,4 +2,12 @@ export type BookType = {
     height: number,
     color: string,
     title: string,
+    author: string,
+    price: number,
+    description: string
+}
+
+export type BookListType = {
+    products: BookType[],
+    success: boolean
 }
